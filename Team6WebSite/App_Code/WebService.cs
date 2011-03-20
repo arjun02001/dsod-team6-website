4e1af9da338fd784bd1f911b5ea61a962cd81497
anwashadhar@gmail.com <anwashadhar@gmail.com@9db364a3-68a1-014d-c5de-ba89a5fb48f1>
2011-03-20 02:31:34 +0000

    Adding GenerateMajorRoutes

--- Team6WebSite/App_Code/WebService.cs
+++ Team6WebSite/App_Code/WebService.cs
@@ -8,6 +8,7 @@ using System.Security.Cryptography;
 using ImageryService;
 using System.Text;
 using System.Text.RegularExpressions;
+using RoutingService;
 
 /// <summary>
 using System.Web.Services;
@@ -267,6 +268,80 @@ public class WebService : System.Web.Services.WebService
             return 0;
         }
     }
+
+
+    [WebMethod]
+    public string GenerateMajorRoutes(string address)
+    {
+        try
+        {
+            string locationString = GetCoordFromAddress(address);
+            string results = "";
+            string key = "AjdBmWL-51nlsoocyGsHAnE3eNOADU1CPWmJ-9kmPL1s7X13jurTlrRx1A49WzwD";
+            MajorRoutesRequest majorRoutesRequest = new MajorRoutesRequest();
+
+            // Set the credentials using a valid Bing Maps key
+            majorRoutesRequest.Credentials = new RoutingService.Credentials();
+            majorRoutesRequest.Credentials.ApplicationId = key;
+
+            // Set the destination of the routes from major roads
+            Waypoint endPoint = new Waypoint();
+            endPoint.Location = new RoutingService.Location();
+            string[] digits = locationString.Split(',');
+            endPoint.Location.Latitude = double.Parse(digits[0].Trim());
+            endPoint.Location.Longitude = double.Parse(digits[1].Trim());
+            endPoint.Description = "Location";
+
+            // Set the option to return full routes with directions
+            MajorRoutesOptions options = new MajorRoutesOptions();
+            options.ReturnRoutes = true;
+
+            majorRoutesRequest.Destination = endPoint;
+            majorRoutesRequest.Options = options;
+
+            // Make the route-from-major-roads request
+            RouteServiceClient routeService = new RouteServiceClient("BasicHttpBinding_IRouteService");
+
+            // The result is an MajorRoutesResponse Object
+            MajorRoutesResponse majorRoutesResponse = routeService.CalculateRoutesFromMajorRoads(majorRoutesRequest);
+
+            Regex regex = new Regex("<[/a-zA-Z:]*>",
+              RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            if (majorRoutesResponse.StartingPoints.Length > 0)
+            {
+                StringBuilder directions = new StringBuilder();
+
+                for (int i = 0; i < majorRoutesResponse.StartingPoints.Length; i++)
+                {
+                    directions.Append(String.Format("Coming from {1}\n", i + 1,
+                        majorRoutesResponse.StartingPoints[i].Description));
+
+                    for (int j = 0;
+                      j < majorRoutesResponse.Routes[i].Legs[0].Itinerary.Length; j++)
+                    {
+                        //Strip tags
+                        string step = regex.Replace(
+                          majorRoutesResponse.Routes[i].Legs[0].Itinerary[j].Text, string.Empty);
+                        directions.Append(String.Format("     {0}. {1}\n", j + 1, step));
+                    }
+                }
+
+                results = directions.ToString();
+            }
+            else
+                results = "No Routes found";
+
+            return results;
+        }
+        catch (Exception)
+        {
+            return "No Routes Found";
+        }
+    }
+   
+
+
 }
 
 
