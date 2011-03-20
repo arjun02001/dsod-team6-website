271a30af482f132f5b46585d2f5abb2fb7016456
anwashadhar@gmail.com <anwashadhar@gmail.com@9db364a3-68a1-014d-c5de-ba89a5fb48f1>
2011-03-20 00:40:50 +0000

    Resolved name space conflicts for GeocodeService and BingGeocode service in Getcoorfromadd

--- Team6WebSite/App_Code/WebService.cs
+++ Team6WebSite/App_Code/WebService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-//using GeocodeService;
+using GeocodeService;
 using BingGeocode;
 
 /// <summary>
@@ -28,22 +28,22 @@ public class WebService : System.Web.Services.WebService {
         {
             CoordinateInfo retVal = null;
             string key = "AjdBmWL-51nlsoocyGsHAnE3eNOADU1CPWmJ-9kmPL1s7X13jurTlrRx1A49WzwD";
-            GeocodeRequest geocodeRequest = new GeocodeRequest();
+           BingGeocode.GeocodeRequest geocodeRequest = new BingGeocode.GeocodeRequest();
 
             // Set the credentials using a valid Bing Maps key
-            geocodeRequest.Credentials = new Credentials();
+            geocodeRequest.Credentials = new BingGeocode.Credentials();
             geocodeRequest.Credentials.ApplicationId = key;
 
             // Set the full address query
             geocodeRequest.Query = address;
 
             // Set the options to only return high confidence results 
-            ConfidenceFilter[] filters = new ConfidenceFilter[1];
-            filters[0] = new ConfidenceFilter();
-            filters[0].MinimumConfidence = Confidence.High;
+            BingGeocode.ConfidenceFilter[] filters = new BingGeocode.ConfidenceFilter[1];
+            filters[0] = new BingGeocode.ConfidenceFilter();
+            filters[0].MinimumConfidence = BingGeocode.Confidence.High;
 
             // Add the filters to the options
-            GeocodeOptions geocodeOptions = new GeocodeOptions();
+            BingGeocode.GeocodeOptions geocodeOptions = new BingGeocode.GeocodeOptions();
             geocodeOptions.Filters = filters;
             geocodeRequest.Options = geocodeOptions;
 
@@ -52,7 +52,7 @@ public class WebService : System.Web.Services.WebService {
             if (geocodeRequest.Query != "")
             {
                 BingGeocode.GeocodeService geocodeService = new BingGeocode.GeocodeService();
-                GeocodeResponse geocodeResponse = geocodeService.Geocode(geocodeRequest);
+                BingGeocode.GeocodeResponse geocodeResponse = geocodeService.Geocode(geocodeRequest);
 
                 if (geocodeResponse.Results.Length > 0)
                 {
@@ -68,6 +68,42 @@ public class WebService : System.Web.Services.WebService {
             return string.Empty;
         }
     }
+
+    //[WebMethod]
+    //public string GetAddressFromCoord(string address)
+    //{
+        //string results = "";
+        //string key = "AjdBmWL-51nlsoocyGsHAnE3eNOADU1CPWmJ-9kmPL1s7X13jurTlrRx1A49WzwD";
+        //ReverseGeocodeRequest reverseGeocodeRequest = new ReverseGeocodeRequest();
+
+        //// Set the credentials using a valid Bing Maps key
+        //reverseGeocodeRequest.Credentials = new GeocodeService1.Credentials();
+        //reverseGeocodeRequest.Credentials.ApplicationId = key;
+
+        //// Set the point to use to find a matching address
+        //GeocodeService1.Location point = new GeocodeService1.Location();
+        //string[] digits = locationString.Split(',');
+
+        //point.Latitude = double.Parse(digits[0].Trim());
+        //point.Longitude = double.Parse(digits[1].Trim());
+
+        //reverseGeocodeRequest.Location = point;
+
+        //// Make the reverse geocode request
+        //GeocodeServiceClient geocodeService = new GeocodeServiceClient("BasicHttpBinding_IGeocodeService");
+        //GeocodeResponse geocodeResponse = geocodeService.ReverseGeocode(reverseGeocodeRequest);
+
+        //if (geocodeResponse.Results.Length > 0)
+        //    results = geocodeResponse.Results[0].DisplayName;
+        //else
+        //    results = "No Results found";
+
+        //return results;
+
+
+
+
+    //}
 }
 
 public class CoordinateInfo
